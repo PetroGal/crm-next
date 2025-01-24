f52f4e3eac836daabf2fd2c5985a6f0bb68b8fd1
PetroGal <69636047+PetroGal@users.noreply.github.com>
2025-01-24 20:10:14 +0200

    created not-found file added MagicBtn

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 import Header from '@/app/components/header';
+import { notFound } from 'next/navigation';
 
 export interface PageProps {
   params: { id: string };
 }
 
-export function generateStaticParams() {
-  return [{ id: '1' }, { id: '2' }, { id: '3' }];
-}
+// export function generateStaticParams() {
+//   return [{ id: '1' }, { id: '2' }, { id: '3' }];
+// }
 
 export default function Page({ params }: PageProps) {
+  useEffect(() => {
+    const id = Number.parseInt(params.id);
+    if (Number.isNaN(id)) {
+      notFound();
+    }
+  }, [params.id]);
+
   return (
     <>
       <Header>Company ({params.id})</Header>
